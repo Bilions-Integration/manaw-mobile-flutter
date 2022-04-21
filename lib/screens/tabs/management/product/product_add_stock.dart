d4cad018b38e119fba126965fdaeab55863da70d
Zeyar Paing <zeyarpaing@ucsy.edu.mm>
2022-04-21 22:34:15 +0630

    add: checkout button in info message

--- lib/screens/tabs/management/product/product_add_stock.dart
+++ lib/screens/tabs/management/product/product_add_stock.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:my_app/components/button.dart';
 import 'package:my_app/data/colors.dart';
 import 'package:my_app/helpers/helper.dart';
 import 'package:my_app/screens/tabs/management/product/components/add_stock_product_item.dart';
@@ -25,16 +26,55 @@ class _AddStockState extends State<ProductAddStock> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: AppColors.dark,
-          title: Text('Add Stock'),
-        ),
-        body: Column(
-          children: [
-            for (var product in productController.purchaseCart.value)
-              AddStockProductItem(product: product)
-          ],
-        ));
+      appBar: AppBar(
+        backgroundColor: AppColors.dark,
+        title: Text('Add Stock'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                for (var product in productController.purchaseCart.value)
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 15, right: 20, top: 12),
+                    child: AddStockProductItem(product: product),
+                  )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.shopping_cart_checkout_outlined,
+                      color: AppColors.grey,
+                    ),
+                    const SizedBox(width: 6),
+                    Text(
+                      'This checkout is for purchasing products',
+                      style: TextStyle(color: AppColors.grey),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                PrimaryButton(
+                    value: 'Checkout',
+                    onPressed: () {
+                      console.log('text');
+                    }),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   _handler({required String action, int? productId}) {}
