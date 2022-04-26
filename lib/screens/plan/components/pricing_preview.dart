43a665a2be10c6cd4d675d496aedddd73a9a5ee4
Zeyar Paing <zeyarpaing@ucsy.edu.mm>
2022-04-26 14:17:58 +0630

    refactor: plan cards'

--- lib/screens/plan/components/pricing_preview.dart
+++ lib/screens/plan/components/pricing_preview.dart
@@ -4,6 +4,7 @@ import 'package:my_app/components/button.dart';
 import 'package:my_app/data/assets.dart';
 import 'package:my_app/data/colors.dart';
 import 'package:my_app/helpers/helper.dart';
+import 'package:my_app/screens/plan/components/plan_card.dart';
 import 'package:my_app/screens/plan/plan_model.dart';
 
 class PricingPreview extends StatefulWidget {
@@ -45,7 +46,6 @@ class _PricingPreview extends State<PricingPreview> {
         SizedBox(
           width: 170,
           child: Container(
-            padding: const EdgeInsets.only(top: 10, bottom: 10),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
               color: AppColors.lightGrey,
@@ -57,27 +57,34 @@ class _PricingPreview extends State<PricingPreview> {
                   onTap: () {
                     _changeTab('monthly');
                   },
-                  child: Text(
-                    "Monthly",
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: selectedTab == 'monthly'
-                            ? FontWeight.bold
-                            : FontWeight.normal),
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(top: 10, bottom: 10, right: 8),
+                    child: Text(
+                      "Monthly",
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: selectedTab == 'monthly'
+                              ? FontWeight.bold
+                              : FontWeight.normal),
+                    ),
                   ),
                 ),
-                mr(1.6),
                 InkWell(
                   onTap: () {
                     _changeTab('yearly');
                   },
-                  child: Text(
-                    'Yearly',
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: selectedTab == 'yearly'
-                            ? FontWeight.bold
-                            : FontWeight.normal),
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(top: 10, bottom: 10, left: 8),
+                    child: Text(
+                      'Yearly',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: selectedTab == 'yearly'
+                              ? FontWeight.bold
+                              : FontWeight.normal),
+                    ),
                   ),
                 ),
               ],
@@ -86,63 +93,15 @@ class _PricingPreview extends State<PricingPreview> {
         ),
         Expanded(
           child: ListView(
-            padding: EdgeInsets.all(15),
+            padding: const EdgeInsets.all(15),
             children: <Widget>[
               ...widget.plans.map((e) {
                 return InkWell(
                   onTap: () => _onPlanSelect(e.name),
-                  child: Container(
-                    margin: const EdgeInsets.only(bottom: 10),
-                    padding: const EdgeInsets.only(
-                        left: 15, right: 15, bottom: 10, top: 10),
-                    decoration: BoxDecoration(
-                      border: selectedPlan == e.name
-                          ? Border.all(color: AppColors.dark, width: 3)
-                          : Border.all(color: Colors.transparent),
-                      borderRadius: BorderRadius.circular(12),
-                      color: AppColors.lightGrey,
-                    ),
-                    child: Row(
-                      children: [
-                        Row(
-                          children: [
-                            Image.asset(
-                              AppAssets.getPlanIcon(e.image),
-                              width: 45,
-                            ),
-                            mr(1),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  e.name,
-                                  style: TextStyle(
-                                      color: e.color,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Row(
-                                  children: [
-                                    Text(e.prices[selectedTab].toString() +
-                                        ' ' +
-                                        e.currency),
-                                    mr(0.5),
-                                    Text(
-                                        '/' + selectedTab.capitalize.toString())
-                                  ],
-                                )
-                              ],
-                            )
-                          ],
-                        ),
-                        InkWell(
-                          onTap: () {},
-                          child: const Text('Plan Details'),
-                        ),
-                      ],
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    ),
-                  ),
+                  child: PlanCard(
+                      plan: e,
+                      selectedPlan: selectedPlan,
+                      selectedTab: selectedTab),
                 );
               })
             ],
