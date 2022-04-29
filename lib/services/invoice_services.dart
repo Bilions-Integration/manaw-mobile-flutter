6195261b4ed59a4dea4886279a44b13577e82720
Zeyar Paing <zeyarpaing@ucsy.edu.mm>
2022-04-29 17:45:53 +0630

    refactor: remove unnecessary in invoice

--- lib/services/invoice_services.dart
+++ lib/services/invoice_services.dart
@@ -1,12 +1,10 @@
 import 'package:my_app/helpers/api.dart';
-import 'package:my_app/helpers/helper.dart';
 import 'package:my_app/model/invoice_model/invoice_data_model.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class InvoiceServices {
   static Future<Map<String, dynamic>> get(Map<String, dynamic> params) async {
-    var res = await Api.get('/invoices', data: params, showLoading : false);
-    console.log('testingbejay $res');
+    var res = await Api.get('/invoices', data: params, showLoading: false);
     var data = InvoiceDataModel.fromJson(res["data"]).toJson();
     return data;
   }
@@ -18,7 +16,7 @@ class InvoiceServices {
   }
 
   static Future create(Map params) async {
-    var res = await Api.post('/invoices/sale', data : params);
+    var res = await Api.post('/invoices/sale', data: params);
   }
 
   // static Future create(Map params) async {
